d115f64cb03df91bea6c31c268e2900d3526b6ed
Michael Wu <mw10013@gmail.com>
2025-01-24 14:25:55 -0500

    jsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
-import type { FC } from 'hono/jsx'
+import type { FC, PropsWithChildren } from 'hono/jsx'
 import { Hono } from 'hono'
 
-const Layout: FC = (props) => {
+const Layout: FC<PropsWithChildren<{}>> = (props) => {
 	return (
 		<html>
+			<head>
+				<meta charset="UTF-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<link href="./tailwind.css" rel="stylesheet" />
+				<title>Hono</title>
+			</head>
 			<body>{props.children}</body>
 		</html>
 	)
 }
 
-const Top: FC<{ messages: string[] }> = (props: { messages: string[] }) => {
+const Top: FC<{ messages: string[] }> = (props) => {
 	return (
 		<Layout>
 			<h1>Hello Hono!</h1>
@@ -27,7 +33,7 @@ export default {
 		const app = new Hono()
 		app.get('/message', (c) => c.text('Hello, World!'))
 		app.get('/random', (c) => c.text(crypto.randomUUID()))
-		app.get('/top', (c) => {
+		app.get('/hono', (c) => {
 			const messages = ['Good Morning', 'Good Evening', 'Good Night']
 			return c.html(<Top messages={messages} />)
 		})
