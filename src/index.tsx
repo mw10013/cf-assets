7066639ccaf074abf3eaa9cf4534eb25f89970c2
Michael Wu <mw10013@gmail.com>
2025-01-24 15:03:28 -0500

    jsx middleware

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import type { FC, PropsWithChildren } from 'hono/jsx'
 import { Hono } from 'hono'
+import { jsxRenderer, useRequestContext } from 'hono/jsx-renderer'
 
 const Layout: FC<PropsWithChildren<{}>> = (props) => {
 	return (
@@ -35,9 +36,26 @@ export default {
 		const app = new Hono()
 		app.get('/message', (c) => c.text('Hello, World!'))
 		app.get('/random', (c) => c.text(crypto.randomUUID()))
+		app.get(
+			'/hono/*',
+			jsxRenderer(({ children }) => {
+				return (
+					<html>
+						<head>
+							<meta charset="UTF-8" />
+							<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+							<link href="./tailwind.css" rel="stylesheet" />
+							<title>Hono JSX</title>
+						</head>
+						<body>{children}</body>
+					</html>
+				)
+			}),
+		)
+
 		app.get('/hono', (c) => {
 			const messages = ['Good Morning', 'Good Evening', 'Good Night']
-			return c.html(<Top messages={messages} />)
+			return c.render(<Top messages={messages} />)
 		})
 
 		return app.fetch(request, env, ctx)
