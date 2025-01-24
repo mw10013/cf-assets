48132bf9ba53a47fb76da57ae0f7247df74d0c72
Michael Wu <mw10013@gmail.com>
2025-01-24 15:13:11 -0500

    card

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,13 @@ import type { FC } from 'hono/jsx'
 import { Hono } from 'hono'
 import { jsxRenderer, useRequestContext } from 'hono/jsx-renderer'
 
-const Top: FC<{ messages: string[] }> = (props) => {
+const Messages: FC<{ messages: string[] }> = (props) => {
 	return (
 		<div className="container prose p-6 lg:prose-xl dark:prose-invert">
 			<h1>Hello Hono!</h1>
 			<ul>
 				{props.messages.map((message) => {
-					return <li>{message}!!</li>
+					return <li>{message}</li>
 				})}
 			</ul>
 		</div>
@@ -18,10 +18,8 @@ const Top: FC<{ messages: string[] }> = (props) => {
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const app = new Hono()
-		app.get('/message', (c) => c.text('Hello, World!'))
-		app.get('/random', (c) => c.text(crypto.randomUUID()))
 		app.get(
-			'/hono/*',
+			'/*',
 			jsxRenderer(({ children }) => {
 				return (
 					<html>
@@ -36,9 +34,22 @@ export default {
 				)
 			}),
 		)
-		app.get('/hono', (c) => {
+		app.get('/', (c) => {
 			const messages = ['Good Morning', 'Good Evening', 'Good Night']
-			return c.render(<Top messages={messages} />)
+			return c.render(<Messages messages={messages} />)
+		})
+		app.get('/card', (c) => {
+			return c.render(
+				<div className="card-border card w-96 bg-base-100">
+					<div className="card-body">
+						<h2 className="card-title">Card Title</h2>
+						<p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+						<div className="card-actions justify-end">
+							<button className="btn btn-primary">Buy Now</button>
+						</div>
+					</div>
+				</div>,
+			)
 		})
 
 		return app.fetch(request, env, ctx)
